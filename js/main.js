f1d2515db72c9936b0407ce9457deb3a75391ecf
Frank Pérez <80561948+Frank-Ish@users.noreply.github.com>
2023-03-04 09:56:41 -0600

    cambios del pull

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-var instance = M.Carousel.init({
-  fullWidth: true,
-  indicators: true
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
 });
 
-// Or with jQuery
-
-$('.carousel.carousel-slider').carousel({
-  fullWidth: true,
-  indicators: true
-});
      
